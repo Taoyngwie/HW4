7562f25ae762142f9da8930da99c7f9dcec44e04
unknown <taoguitardz@gmail.com>
2023-04-30 21:17:57 +0700

    1

---
+++ H1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Graph
+{
+    private Dictionary<int, List<int>> adjacencyList;
+    private int numNodes;
+
+    public Graph(int numNodes)
+    {
+        this.numNodes = numNodes;
+        adjacencyList = new Dictionary<int, List<int>>();
+    }
+
+    public void AddEdge(int source, int destination)
+    {
+        if (!adjacencyList.ContainsKey(source))
+        {
+            adjacencyList[source] = new List<int>();
+        }
+
+        adjacencyList[source].Add(destination);
+    }
+
+    public bool IsReachable(int source, int destination)
+    {
+        bool[] visited = new bool[numNodes];
+        DFS(source, visited);
+
+        return visited[destination];
+    }
+
+    private void DFS(int node, bool[] visited)
+    {
+        visited[node] = true;
+
+        if (!adjacencyList.ContainsKey(node)) return;
+
+        foreach (int neighbor in adjacencyList[node])
+        {
+            if (!visited[neighbor])
+            {
+                DFS(neighbor, visited);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of nodes in the graph: ");
+        int numNodes = int.Parse(Console.ReadLine());
+
+        Graph graph = new Graph(numNodes);
+
+        Console.WriteLine("Enter edges as pairs of nodes (0 <= i,j < n) (negative or out of range values to stop):");
+        while (true)
+        {
+            Console.Write("Source: ");
+            int source = int.Parse(Console.ReadLine());
+            if (source < 0 || source >= numNodes) break;
+
+            Console.Write("Destination: ");
+            int destination = int.Parse(Console.ReadLine());
+            if (destination < 0 || destination >= numNodes) break;
+
+            graph.AddEdge(source, destination);
+        }
+
+        Console.WriteLine("Enter pairs of nodes to check reachability (0 <= i,j < n):");
+        while (true)
+        {
+            Console.Write("Source: ");
+            int source = int.Parse(Console.ReadLine());
+            if (source < 0 || source >= numNodes) break;
+
+            Console.Write("Destination: ");
+            int destination = int.Parse(Console.ReadLine());
+            if (destination < 0 || destination >= numNodes) break;
+
+            if (graph.IsReachable(source, destination))
+            {
+                Console.WriteLine("Reachable");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Unreachable");
+                break;
+            }
+        }
+    }
+}
---
+++ H2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n; // Number of nodes
+        Console.Write("Number of nodes: ");
+        n = int.Parse(Console.ReadLine());
+
+        // Create a dictionary to store edges between nodes
+        Dictionary<int, List<int>> adjacencyList = new Dictionary<int, List<int>>();
+
+        // Receive pairs of connected nodes
+        int i, j;
+        Console.WriteLine("Enter pairs of connected nodes (end by entering negative or out-of-range numbers 0-" + (n - 1) + "):");
+        while (true)
+        {
+            Console.Write("Source node: ");
+            i = int.Parse(Console.ReadLine());
+            if (i < 0 || i >= n) break;
+
+            Console.Write("Destination node: ");
+            j = int.Parse(Console.ReadLine());
+            if (j < 0 || j >= n) break;
+
+            AddEdge(adjacencyList, i, j);
+            AddEdge(adjacencyList, j, i);
+        }
+
+        // Calculate the minimum number of symbols needed
+        bool[] used = new bool[n];
+        int symbolCount = 0;
+        for (i = 0; i < n; i++)
+        {
+            if (!used[i])
+            {
+                used[i] = true;
+                symbolCount++;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (!used[j] && !AreConnected(adjacencyList, i, j))
+                    {
+                        used[j] = true;
+                    }
+                }
+            }
+        }
+
+        // Display the result
+        Console.WriteLine("Minimum number of symbols needed: " + symbolCount);
+    }
+
+    private static void AddEdge(Dictionary<int, List<int>> adjacencyList, int source, int destination)
+    {
+        if (!adjacencyList.ContainsKey(source))
+        {
+            adjacencyList[source] = new List<int>();
+        }
+
+        adjacencyList[source].Add(destination);
+    }
+
+    // Check if nodes i and j are connected
+    private static bool AreConnected(Dictionary<int, List<int>> adjacencyList, int i, int j)
+    {
+        if (adjacencyList.ContainsKey(i) && adjacencyList[i].Contains(j))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
